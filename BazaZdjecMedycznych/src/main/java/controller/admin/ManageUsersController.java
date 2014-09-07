d4f96e386b970ea09d0ef7d8429b6a23209535b3
Przemyslaw Thomann <peerzet3@gmail.com>
2014-09-07 14:59:14 +0100

    + Checkbox and Button in table

--- BazaZdjecMedycznych/src/main/java/controller/admin/ManageUsersController.java
+++ BazaZdjecMedycznych/src/main/java/controller/admin/ManageUsersController.java
@@ -5,23 +5,41 @@
  */
 package controller.admin;
 
+import com.mysql.jdbc.RowData;
 import controller.Window;
+import java.awt.Checkbox;
 import java.net.URL;
+import java.sql.Array;
+import java.sql.SQLException;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.ResourceBundle;
+import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.property.ReadOnlyObjectWrapper;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableColumn.CellDataFeatures;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.CheckBoxTableCell;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.layout.BorderPane;
+import javafx.util.Callback;
 import model.ResourceBundleMaster;
 import model.StageMaster;
 import model.db.DBUsersManager;
+import model.enums.UserType;
 import model.tableentries.UserEntry;
 
 /**
@@ -37,8 +55,6 @@ public class ManageUsersController extends Window {
     @FXML
     private TableView usersTableView;
     @FXML
-    private TableColumn idTableColumn;
-    @FXML
     private TableColumn usernameTableColumn;
     @FXML
     private TableColumn passwordTableColumn;
@@ -48,11 +64,16 @@ public class ManageUsersController extends Window {
     private Button addUserButton;
     @FXML
     private BorderPane manageUsersBorderPane;
+    @FXML
+    private TableColumn checkboxTableColumn;
+    @FXML
+    private TableColumn buttonTableColumn;
     private DBUsersManager usersMaster = new DBUsersManager();
+    int i = 0;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        rb=ResourceBundleMaster.TRANSLATOR.getResourceBundle();
+        rb = ResourceBundleMaster.TRANSLATOR.getResourceBundle();
         initButtons();
         linkTableColumns();//after it we will can paste UserEntry to the table
         fillUsersTable();
@@ -61,23 +82,66 @@ public class ManageUsersController extends Window {
     private void fillUsersTable() {
         usersMaster.updateResultSet();
         ObservableList data = FXCollections.observableArrayList();
-        HashMap<Integer, String> usernames = usersMaster.readUsernames();
-        HashMap<Integer, String> passwords = usersMaster.readPasswords();
-        HashMap<Integer, String> accountTypes = usersMaster.readAccountTypes();
-        for (int i = 1; i < usernames.size() + 1; i++) { //from 1 because we use id in table from 1
-            String username = usernames.get(i);
-            String password = passwords.get(i);
-            String accountType = accountTypes.get(i);
-            data.add(new UserEntry(i, username, password, accountType));
+        HashMap<String, String> usernames = usersMaster.readUsernames();
+        HashMap<String, String> passwords = usersMaster.readPasswords();
+        HashMap<String, String> accountTypes = usersMaster.readAccountTypes();
+        Set<String> usernameSet = usernames.keySet();
+        for (String user : usernameSet) { //from 1 because we use id in table from 1
+            String username = usernames.get(user);
+            String password = passwords.get(user);
+            String accountType = accountTypes.get(user);
+            data.add(new UserEntry(username, password, accountType, new Boolean(true)));
         }
         usersTableView.setItems(data);
+        usersTableView.setEditable(true);
     }
 
     private void linkTableColumns() {
-        idTableColumn.setCellValueFactory(new PropertyValueFactory<UserEntry, Integer>("id"));
         usernameTableColumn.setCellValueFactory(new PropertyValueFactory<UserEntry, String>("username"));
         passwordTableColumn.setCellValueFactory(new PropertyValueFactory<UserEntry, String>("password"));
         accountTypeTableColumn.setCellValueFactory(new PropertyValueFactory<UserEntry, String>("accountType"));
+        buttonTableColumn.setCellValueFactory(new PropertyValueFactory<UserEntry, String>("username"));
+        buttonTableColumn.setCellFactory(new Callback<TableColumn<UserEntry, String>, TableCell<UserEntry, String>>() {
+
+            @Override
+            public TableCell<UserEntry, String> call(TableColumn<UserEntry, String> param) {
+                TableCell<UserEntry, String> cell = new TableCell<UserEntry, String>() {
+                    final Button button = new Button(ResourceBundleMaster.TRANSLATOR.getTranslation("information"));
+
+                    {
+                        button.setMinWidth(160);
+                        button.setVisible(true);
+                        setGraphic(button);//kurwa mać 1h w dupie bo tego nie było
+                        button.setOnAction(event -> {
+                            System.out.println(param);
+                            System.out.println(param.getCellData(getIndex()));
+                        });
+                    }
+
+                };
+                cell.setAlignment(Pos.CENTER);
+                return cell;
+            }
+
+        });
+        checkboxTableColumn.setCellValueFactory(new PropertyValueFactory<UserEntry, Boolean>("selected"));
+        checkboxTableColumn.setCellFactory(new Callback<TableColumn<UserEntry, Boolean>, TableCell<UserEntry, Boolean>>() {
+
+            @Override
+            public TableCell<UserEntry, Boolean> call(TableColumn<UserEntry, Boolean> param) {
+                CheckBoxTableCell cell = new CheckBoxTableCell(){
+
+                    @Override
+                    public void updateItem(Object item, boolean empty) {
+                        super.updateItem(item, empty); //To change body of generated methods, choose Tools | Templates.
+                        System.out.println("kon rafal ");
+                    }
+                    
+                };
+//                cell.setAlignment(Pos.CENTER);
+                return cell;
+            }
+        });
     }
 
     private void initButtons() {
@@ -95,10 +159,15 @@ public class ManageUsersController extends Window {
 //                MainWindowAdminController mainWindowAdminController = fxmlLoader.<MainWindowAdminController>getController();
 //                mainWindowAdminController.getBorderPane().setCenter(manageUsersRoot);
                 showWindow("admin/AddUser.fxml");
-                
 
             }
         }
         );
+
     }
+
+    public class CheckBoxCell extends CheckBoxTableCell<UserType, Boolean> {
+
+    }
+
 }
